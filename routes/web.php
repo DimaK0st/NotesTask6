a03cbd950aee3fe820cd073e65d9976b3dbfe646
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2021-07-10 15:10:29 +0300

    Start job

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +14,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/user/{id}/{name}', function ($id, $name) {
+    return view('about');
 });
